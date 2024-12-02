b906083a1a641dffe8ee3e4bf9a5a0c0c42dc138
mk0904 <mk10092004@gmail.com>
2024-12-02 11:57:52 +0530

    routes updated migrations done

--- routes/shippingRoutes.js
+++ routes/shippingRoutes.js
@@ -26,27 +26,30 @@ router.put("/cancel", async (req, res) => {
       error: "Missing shippingId",
     });
   }
-  try{
-  const product = await prisma.shipping.update({
-    where: { id: Number(shippingId) },
-    data: { status: "cancelled" },
-  });
-  return res.status(200).json(product);}
-  finally{
-     return res.status(404).json({
-          error: "Missing shippingId",
-        });
+  try {
+    const product = await prisma.shipping.update({
+      where: { id: Number(shippingId) },
+      data: { status: "cancelled" },
+    });
+    return res.status(200).json(product);
+  } finally {
+    return res.status(404).json({
+      error: "Missing shippingId",
+    });
   }
 });
 
 router.get("/get", async (req, res) => {
-  let userId = req.params;
-  userId = userId.strip("=");
-  console.log(userId);
-  console.log(typeof req.url);
-  const product = await prisma.shipping.findMany();
+  let {userId} = req.query;
+  if (userId !== undefined){
+     const product = await prisma.shipping.findUnique({where:{userId: Number(userId)}});
+     return res.status(200).json(product)
+  }
+     const product = await prisma.shipping.findMany();
   return res.status(200).json(product);
-});
+  }
+  
+);
 
 router.get("/get/:userId", async (req, res) => {
   const userId = req.path;
